7c1e09e709859dae9100117a8234be8042c0476a
김덕현 <oraclian@estsecurity.com>
2022-11-21 16:08:52 +0900

    프로젝트 기본 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Docs')
+    .setDescription('Boilerplate for admin')
+    .setVersion('0.0.1')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    Logger.verbose(`🚀 Server is Started!\r\n`);
+    Logger.verbose(`📌 Local: http://localhost:${port}`);
+    Logger.verbose(`📍 Local: http://0.0.0.0:${port}`);
+  });
 }
 bootstrap();
